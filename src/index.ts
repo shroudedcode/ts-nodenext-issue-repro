0f2b76a59f8837d0147a4575815ca899fad6163b
Niklas Higi <niklas@shroudedcode.com>
2022-02-08 18:28:35 +0100

    Add comment about expected error

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,6 @@
+// The line below should show a "Relative import paths need explicit file
+// extensions..." error in VS Code, but it doesn't. The error is only picked up
+// by `tsc` which seems to properly infer the module type.
 import { helloWorld } from './example'
 
 helloWorld()
